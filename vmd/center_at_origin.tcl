b97d70a4703b1c406b6756fd99da65534837cde9
Victoria Lim <lim.t.victoria@gmail.com>
2019-05-10 14:52:13 -0700

    updates from gpl

--- vmd/center_at_origin.tcl
+++ vmd/center_at_origin.tcl
@@ -1,20 +1,30 @@
-
+#
+# center_at_origin.tcl
+#
 # Purpose: Center enter system at origin.
+#
 # Usage:
-# > While in VMD:
-#       [0] might need to fix source location of move_atoms.tcl
-#       [1] source center_at_origin.tcl
-#       [2] recenter OR recenter all 0 (where all can be replaced by desired selection text, and 0 can be replaced by molid
-# > On command line:
-#       * TODO, not yet supported
+# [1] If in VMD interactive:
+#     1. source center_at_origin.tcl
+#     2. recenter
+#        OR
+#        recenter all 0 (where 'all' can be replaced by vmd selection text, and 0 can be replaced by molid)
+#
+# [2] If on command line:
+#     1. vmd -dispdev none file.pdb -e ../../center_at_origin.tcl -args all 0
+#
 # Full path names for sourcing:
 #   gpl: /DFS-L/DATA/mobley/limvt/gitmisc/vmd/center_at_origin.tcl
 #   cas: /home/limvt/connect/greenplanet/goto-cluster/gitmisc/vmd/center_at_origin.tcl
+#
 # By:       Victoria T. Lim
-# Version:  Nov 29 2018
+# Version:  May 6 2019
+#
+# Notes:
+# - May need to fix location of the dependency script (move_atoms.tcl)
 
-#source move_atoms.tcl
-source /home/limvt/connect/greenplanet/goto-cluster/gitmisc/vmd/move_atoms.tcl
+source ../../move_atoms.tcl
+#source /home/limvt/connect/greenplanet/goto-cluster/gitmisc/vmd/move_atoms.tcl
 
 proc recenter { {seltext "all"} {molid 0} } {
     set sel [atomselect $molid $seltext]
@@ -23,3 +33,17 @@ proc recenter { {seltext "all"} {molid 0} } {
     moveby $sel $vecmove
 }
 
+if {$argc > 0} {
+
+    # grab molid argument
+    if {$argc == 2} {
+        set molid [lindex $argv 1]
+    } else {
+        set molid 0
+    }
+
+    set censel [lindex $argv 0]
+    recenter $censel $molid
+    animate write pdb centered_output.pdb sel [atomselect $molid all] $molid
+    exit
+}
